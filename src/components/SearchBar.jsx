45d3fe1ee0b4edc83c9ed4f1b5b1f01f3e332928
ValentinoMartz <valentinomartinez55@gmail.com>
2022-09-02 17:51:36 -0300

    Search working

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,14 +1,26 @@
-import { IconButton, Paper } from "@mui/material";
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { Search } from "@mui/icons-material";
+import { Paper, IconButton } from "@mui/material";
+import SearchIcon from "@mui/icons-material/Search";
+
 const SearchBar = () => {
-  useState();
+  const [searchTerm, setSearchTerm] = useState("");
   const navigate = useNavigate();
+
+  const onhandleSubmit = (e) => {
+    e.preventDefault();
+
+    if (searchTerm) {
+      navigate(`/search/${searchTerm}`);
+
+      setSearchTerm("");
+    }
+  };
+
   return (
     <Paper
       component="form"
-      onSubmit={() => {}}
+      onSubmit={onhandleSubmit}
       sx={{
         borderRadius: 20,
         border: "1px solid #e3e3e3",
@@ -18,14 +30,17 @@ const SearchBar = () => {
       }}
     >
       <input
-        type="text"
         className="search-bar"
         placeholder="Search..."
-        value=""
-        onChang={() => {}}
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
       />
-      <IconButton type="submit" sx={{ p: "10px", color: "red" }}>
-        <Search />
+      <IconButton
+        type="submit"
+        sx={{ p: "10px", color: "red" }}
+        aria-label="search"
+      >
+        <SearchIcon />
       </IconButton>
     </Paper>
   );
